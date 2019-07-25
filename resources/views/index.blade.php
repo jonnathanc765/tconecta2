3586048b7941f279b3f635a9b16f00dacf5ed9cf
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2019-07-25 17:10:05 -0400

    reduccion de peso de la imagen

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,7 +11,7 @@
         <div class="row">
 
             <div class="col-md-6 col-lg-6 d-flex justify-content-center align-items-end secundary-banner">
-                <img src="images/banner-secundario.png" alt="">
+                <img src="{{ asset('images/banner-secundario.png') }}" alt="">
             </div>
 
             <div class="col-md-6 col-lg-6">
@@ -52,14 +52,14 @@
 
         <div class="row my-5 img-title">
             <div class="col-md-12 text-center">
-                <img src="images/internet-siempre.png" alt="">
+                <img src="{{ asset('images/internet-siempre.png') }}" alt="">
             </div>
         </div>
 
         <div class="row mt-4 resources">
             <div class="col-md-4 resource-item" data-aos="fade-right">
                 <div class="text-center">
-                    <img src="images/icons/A-PRUEBA-DE.png" alt="">
+                    <img src="{{ asset('images/icons/A-PRUEBA-DE.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>a prueba de fallas eléctricas</h4>
@@ -69,7 +69,7 @@
 
             <div class="col-md-4 resource-item" data-aos="fade-left">
                 <div class="text-center">
-                    <img src="images/icons/SERVICIO-TECNICO.png" alt="">
+                    <img src="{{ asset('images/icons/SERVICIO-TECNICO.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>Servicio técnico garantizado</h4>
@@ -80,7 +80,7 @@
 
             <div class="col-md-4 resource-item" data-aos="fade-right">
                 <div class="text-center">
-                    <img src="images/icons/VELOCIDAD.png" alt="">
+                    <img src="{{ asset('images/icons/VELOCIDAD.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>Velocidad simétrica</h4>
@@ -90,7 +90,7 @@
 
             <div class="col-md-4 resource-item" data-aos="fade-left">
                 <div class="text-center">
-                    <img src="images/icons/INCLUYE.png" alt="">
+                    <img src="{{ asset('images/icons/INCLUYE.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>Incluye linea telefónica</h4>
@@ -99,7 +99,7 @@
             </div>
             <div class="col-md-4 resource-item" data-aos="fade-right">
                 <div class="text-center">
-                    <img src="images/icons/INSTALACION-EN.png" alt="">
+                    <img src="{{ asset('images/icons/INSTALACION-EN.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>Instalacion en tiempo record</h4>
@@ -108,7 +108,7 @@
             </div>
             <div class="col-md-4 resource-item" data-aos="fade-left">
                 <div class="text-center">
-                    <img src="images/icons/AVALADOS.png" alt="">
+                    <img src="{{ asset('images/icons/AVALADOS.png') }}" alt="">
                 </div>
                 <div class="captions">
                     <h4>Avalado por los grandes</h4>
@@ -148,7 +148,7 @@
                         </a>
                     </div>
                     <div class="col-md-6 d-flex justify-content-center align-items-center mt-4">
-                        <img src="images/logo.png" alt="">
+                        <img src="{{ asset('images/logo.png') }}" alt="">
                     </div>
                 </div>
             </div>
@@ -157,104 +157,3 @@
     </div>
 </footer>
 @endsection
-
-
-{{-- <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html> --}}
