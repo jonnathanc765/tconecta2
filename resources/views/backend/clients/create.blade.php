4ce93d99da3a84c428a398eaa89176b21519fe72
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2019-07-29 12:29:04 -0400

    correcion

--- resources/views/backend/clients/create.blade.php
+++ resources/views/backend/clients/create.blade.php
@@ -8,44 +8,49 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+            <div class="card">
 
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="user">Usuario Instagram</label>
-                    <input type="text" class="form-control" name="user">
+                <div class="card-body">
+                    <h2>Agregar un nuevo Cliente</h2>
+                    <div class="form-group">
+                        <label for="user">Usuario Instagram</label>
+                        <input type="text" class="form-control" name="user">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Nombre</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="user">Usuario Instagram</label>
+                        <input type="text" class="form-control" name="user">
+                    </div>
+                    <div class="form-group">
+                        <label for="user">Usuario Instagram</label>
+                        <input type="text" class="form-control" name="user">
+                    </div>
+                    <div class="form-group">
+                        <label for="lead">Contactado desde</label>
+                        <select name="lead" id="lead" class="form-control">
+                            <option value="" disabled selected>Seleccione</option>
+                            <option value="Facebook">Facebook</option>
+                            <option value="Instragram">Instragram</option>
+                            <option value="WhatsApp">WhatsApp</option>
+                            <option value="DM">Mensaje directo</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Nombre</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="form-group">
+                        <label for="user">Usuario Instagram</label>
+                        <input type="text" class="form-control" name="user">
+                    </div>
+                    <button type="submit">Agregar</button>
                 </div>
-                <div class="form-group">
-                    <label for="name">Nombre</label>
-                    <input type="text" class="form-control" name="name">
-                </div>
-                <div class="form-group">
-                    <label for="user">Usuario Instagram</label>
-                    <input type="text" class="form-control" name="user">
-                </div>
-                <div class="form-group">
-                    <label for="user">Usuario Instagram</label>
-                    <input type="text" class="form-control" name="user">
-                </div>
-                <div class="form-group">
-                    <label for="lead">Contactado desde</label>
-                    <select name="lead" id="lead">
-                        <option value="" disabled selected>Seleccione</option>
-                        <option value="Facebook">Facebook</option>
-                        <option value="Instragram">Instragram</option>
-                        <option value="WhatsApp">WhatsApp</option>
-                        <option value="DM">Mensaje directo</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="name">Nombre</label>
-                    <input type="text" class="form-control" name="name">
-                </div>
-                <div class="form-group">
-                    <label for="user">Usuario Instagram</label>
-                    <input type="text" class="form-control" name="user">
-                </div>
-                <button type="submit">Agregar</button>
             </div>
         </div>
     </div>
 </div>
+
+@endsection
